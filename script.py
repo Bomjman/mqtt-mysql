01482d0b9752390af30793f5a933634d3518ba39
Daniel Presl <igogosha@tutanota.com>
2021-07-12 16:03:00 +0300

    changed on_message func

--- script.py
+++ script.py
@@ -7,64 +7,27 @@ import mysql.connector as mysql
 
 db = mysql.connect( user='mqtt', password='secret', database='mydb')
 cursor = db.cursor()
-broker_topic = [("hello/#", 0), ("/#", 0)]
+broker_topic = [("Bailey001", 0)]
 
 def on_connect(client, userdata, flags, rc):
     print("rc: " + str(rc))
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    vars_to_sql = []
-    keys_to_sql = []
-    list = []
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload.decode('utf-8')))
 
-    list = json.loads(msg.payload)
-
-    for key, value in list.iteritems():
-        print("")
-        print(key, value)
-        if key == 'tst':
-            print("time found")
-            print (value)
-            value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(value)))
-            print (value)
-
-        value_type = type(value)
-        if value_type is not dict:
-            print("value_type is not dict")
-            if value_type is unicode:
-                print("value_type is unicode")
-                vars_to_sql.append(value.encode('ascii', 'ignore'))
-                keys_to_sql.append(key.encode('ascii', 'ignore'))
-            else:
-                print("value_type is not unicode")
-                vars_to_sql.append(value)
-                keys_to_sql.append(key)
-
-    print("topic", msg.topic)
-    addtopic = 'topic'
-    vars_to_sql.append(msg.topic.encode('ascii', 'ignore'))
-    keys_to_sql.append(addtopic.encode('ascii', 'ignore'))
-
-    keys_to_sql = ', '.join(keys_to_sql)
+    sql = "INSERT INTO topics(topic, msg) VALUES(%s,%s)"
 
     try:
-       # Execute the SQL command
-       # change locations to the table you are using
-       queryText = "INSERT INTO locations(%s) VALUES %r"
-       queryArgs = (keys_to_sql, tuple(vars_to_sql))
-       cursor.execute(queryText % queryArgs)
-       print('Successfully Added record to mysql')
-       db.commit()
-
-    except mysql.connector.Error as e:
-        try:
-            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-        except IndexError:
-            print("MySQL Error: %s" % str(e))
-        # Rollback in case there is any error
+        cursor.execute(sql, [msg.topic, str(msg.payload.decode('utf-8'))])
+        print('Successfully Added record to mysql')
+        db.commit()
+    except db.Error as error:
+        print("Error: {}".format(error))
         db.rollback()
-        print('ERROR adding record to MYSQL')
+
+#def on_publish(mosq, obj, mid):
+#    print("mid: "+str(mid))
+
 
 def on_subscribe(client, obj, mid, granted_qos):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
@@ -72,6 +35,7 @@ def on_subscribe(client, obj, mid, granted_qos):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+#client.on_publish = on_publish
 client.on_subscribe = on_subscribe
 
 # Parse CLOUDMQTT_URL
@@ -82,10 +46,10 @@ url = urlparse(url_str)
 client.username_pw_set(url.username, url.password)
 client.connect(url.hostname, url.port)
 
-# an array with some topics
 # Start subscribe, with QoS level 0
 client.subscribe(broker_topic)
 
+#client.publish("Bailey001", "hello 123")
 # Continue the network loop
 rc = 0
 while rc == 0:
