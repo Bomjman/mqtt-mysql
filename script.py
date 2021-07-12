10fde61c552cb4231282796f967c77659643e355
Daniel Presl <igogosha@tutanota.com>
2021-07-12 19:00:11 +0300

    add date and time to the db

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, json, time
+import os, time
 import paho.mqtt.client as mqtt
 from urllib.parse import urlparse
 import mysql.connector as mysql
@@ -15,18 +15,19 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload.decode('utf-8')))
 
-    sql = "INSERT INTO topics(topic, msg) VALUES(%s,%s)"
+    sql = "INSERT INTO topics(topic, msg, time) VALUES(%s,%s, %s)"
+    datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 
     try:
-        cursor.execute(sql, [msg.topic, str(msg.payload.decode('utf-8'))])
+        cursor.execute(sql, [msg.topic, str(msg.payload.decode('utf-8')), datetime])
         print('Successfully Added record to mysql')
         db.commit()
-    except db.Error as error:
+    except mysql.Error as error:
         print("Error: {}".format(error))
         db.rollback()
 
-#def on_publish(mosq, obj, mid):
-#    print("mid: "+str(mid))
+# def on_publish(mosq, obj, mid):
+#     print("mid: "+str(mid))
 
 
 def on_subscribe(client, obj, mid, granted_qos):
@@ -49,7 +50,7 @@ client.connect(url.hostname, url.port)
 # Start subscribe, with QoS level 0
 client.subscribe(broker_topic)
 
-#client.publish("Bailey001", "hello 123")
+#client.publish("Bailey001", "my local time")
 # Continue the network loop
 rc = 0
 while rc == 0:
